a5529c81523ffa9139888f1cbaf014b68540f86a
Jeonghun Ha <jhhm0315@gmail.com>
2025-02-19 13:46:27 +0900

    evoformer:init stacks structures

---
+++ Highly accurate protein structure prediction with AlphaFold/Stacks/Evoformer.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn as nn
+
+class MSA_row_wise_gated_self_attention_with_pair_bias(nn.Module):
+    def __init__(self,):
+        super(MSA_row_wise_gated_self_attention_with_pair_bias, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class MSA_column_wise_gated_self_attention(nn.Moudule):
+    def __init__(self,):
+        super(MSA_column_wise_gated_self_attention, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class MSA_translation(nn.Module):
+    def __init__(self,):
+        super(MSA_translation, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class Outer_product_mean(nn.Module):
+    def __init__(self,):
+        super(Outer_product_mean, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class Triangular_multiplicative_update(nn.Module):
+    def __init__(self,):
+        super(Triangular_multiplicative_update, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class Triangular_self_attention(nn.Module):
+    def __init__(self,):
+        super(Triangular_self_attention, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
+
+
+class Translation_in_the_pair_stack(nn.Module):
+    def __init__(self,):
+        super(Translation_in_the_pair_stack, self).__init__()
+        pass
+
+    def forward(self,):
+        pass
