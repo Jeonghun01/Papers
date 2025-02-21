524c907521c0a7e79a0dd3b5830b10626974fe43
Jeonghun Ha <jhhm0315@gmail.com>
2025-02-21 14:36:32 +0900

    base:structure module

--- Highly accurate protein structure prediction with AlphaFold/Stacks/Structure_Module.py
+++ Highly accurate protein structure prediction with AlphaFold/Stacks/Structure_Module.py
@@ -1,2 +1,71 @@
 import torch
 import torch.nn as nn
+
+
+s           = 50  # random
+r           = 100 # random
+c_m         = 256 # on paper
+c_z         = 128 # on paper
+
+
+"""
+       
+"""
+
+
+class rigidFrom3Points(nn.Module):
+    def __init__(self,):
+        super(rigidFrom3Points, self).__init__()
+
+    def forward(self,):
+        pass
+
+
+
+
+class InvariantPointAttention(nn.Module):
+    def __init__(self,):
+        super(InvariantPointAttention, self).__init__()
+
+    def forward(self,):
+        pass
+
+
+
+
+class BackboneUpdate(nn.Module):
+    def __init__(self,):
+        super(BackboneUpdate, self).__init__()
+
+    def forward(self,):
+        pass
+
+
+
+
+class computeAllAtomCoordinates(nn.Module):
+    def __init__(self,):
+        super(computeAllAtomCoordinates, self).__init__()
+
+    def forward(self,):
+        pass
+
+
+
+
+class makeRotX(nn.Module):
+    def __init__(self,):
+        super(makeRotX, self).__init__()
+
+    def forward(self,):
+        pass
+
+
+
+
+class renameSymmetricGroundTruthAtoms(nn.Module):
+    def __init__(self,):
+        super(renameSymmetricGroundTruthAtoms, self).__init__()
+
+    def forward(self,):
+        pass
