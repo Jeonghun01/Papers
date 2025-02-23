6f2f09e646095cff7506147a0361bbb22b11fcc2
Jeonghun Ha <jhhm0315@gmail.com>
2025-02-23 15:26:49 +0900

    update:computeFAPE

--- Highly accurate protein structure prediction with AlphaFold/Loss.py
+++ Highly accurate protein structure prediction with AlphaFold/Loss.py
@@ -6,40 +6,45 @@ import numpy as np
 
 
 
-class torsionAngleLoss(nn.Module):
-    def __init__(self,):
-        super(torsionAngleLoss, self).__init__()
 
-    def forward(self, angles, angles_t, angles_at):
-        l_list = []
-        for i in range(len(angles)):
-            l_list[i] = torch.norm(angles[i])
-            angles[i] = angles[i] / l_list[i]
+
+def torsionAngleLoss(angles, angles_t, angles_at):
+    l_list = []
+    angles_n = ()
+
+    for i in range(len(angles)):
+        l_list.append(torch.norm(angles[i]))
+        angles_n =  angles_n + (angles[i] / l_list[i],)
             
-        L_torsion_list = []
-        L_anglenorm_list = []
+    L_torsion_list = []
+    L_anglenorm_list = []
 
-        for i in range(len(angles)):
-            L_torsion_f = torch.min(torch.pow(torch.norm(angles[i] - angles_t[i]),2), torch.norm(angles[i] - angles_at[i]))
-            L_anglenorm_f = np.abs(l_list[i] - 1)
+    for i in range(len(angles)):
+        L_torsion_f = torch.minimum(torch.pow(torch.norm(angles_n[i] - angles_t[i]),2), torch.norm(angles_n[i] - angles_at[i]))
+        L_anglenorm_f = np.abs(l_list[i] - 1)
 
-            L_torsion_list.append(L_torsion_f)
-            L_anglenorm_list.append(L_anglenorm_f)
+        L_torsion_list.append(L_torsion_f)
+        L_anglenorm_list.append(L_anglenorm_f)
         
-        L_torsion = np.mean(L_torsion_list)
-        L_anglenorm = np.mean(L_anglenorm_list)
+    L_torsion = np.mean(L_torsion_list)
+    L_anglenorm = np.mean(L_anglenorm_list)
 
-        return L_torsion + 0.02 * L_anglenorm
-
-
+    return L_torsion + 0.02 * L_anglenorm
 
 
-class computeFAPE(nn.Module):
-    def __init__(self,):
-        super(computeFAPE, self).__init__()
 
-    def forward(self,):
-        pass
+
+def computeFAPE(T, x, T_t, x_t, Z = 1e-9, d_clamp = 1e-9, epsilon = 1e-22):
+    x = torch.matmul((1/T[0]), x.T).transpose(1,2) - torch.matmul((1/T[0]), T[1].T).transpose(1,2)
+    x_t = torch.matmul((1/T_t[0]), x_t.T).transpose(1,2) + torch.matmul((1/T_t[0]), T_t[1].T).transpose(1,2)
+
+    d = np.sqrt(torch.pow(torch.norm(x - x_t), 2).numpy().item() + epsilon)
+    L_FAPE = (1 / Z) * np.mean(np.minimum(d_clamp, d))
+
+    return L_FAPE
+
+    
+        
 
 
 
